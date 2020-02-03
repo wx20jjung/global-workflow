3cd56834389809dc52abb3ce9723db6c37bf772f
wx20jjung <Jim.Jung@noaa.gov>
2020-02-03 13:39:40 +0000

    changes for s4 to gaussian_sfcanl.fd and nst_tf_chg.fd

--- sorc/gaussian_sfcanl.fd/makefile.sh
+++ sorc/gaussian_sfcanl.fd/makefile.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-export FFLAGS="-O3 -fp-model precise -g -r8 -i4"
+export FFLAGS="-O2 -fp-model strict -g -r8 -i4 -traceback"
 # for debugging
 #export FFLAGS="-g -r8 -i4 -warn unused -check bounds"
 
-export NETCDF_INCLUDE="-I${NETCDF}/include"
-export NETCDF_LDFLAGS_F="-L${NETCDF}/lib -lnetcdf -lnetcdff -L${HDF5}/lib -lhdf5 -lhdf5_fortran"
+#export NETCDF_INCLUDE="-I${NETCDF}/include"
+export NETCDF_INCLUDE="-I${NETCDF_INC}"
+#export NETCDF_LDFLAGS_F="-L${NETCDF}/lib -lnetcdf -lnetcdff -L${HDF5}/lib -lhdf5 -lhdf5_fortran"
+export NETCDF_LDFLAGS_F="-L${NETCDF_LIB} -lnetcdf -lnetcdff"
 
 make clean
 make build
